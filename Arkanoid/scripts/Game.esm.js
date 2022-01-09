398c642521af9bb945e1e1a321c9a17ae1be7f82
KroMateusz <81087684+KroMateusz@users.noreply.github.com>
2022-01-09 20:19:47 +0100

    Add files via upload

---
+++ Arkanoid/scripts/Game.esm.js
@@ -0,0 +1,146 @@
+import {
+	Common,
+	VISIBLE_SCREEN
+} from './Common.esm.js';
+import {
+	DATALOADED_EVENT_NAME
+} from './Loader.esm.js';
+import {
+	gameLevels,
+} from './gameLevels.esm.js';
+import {
+	canvas
+} from './Canvas.esm.js';
+import {
+	media
+} from './Media.esm.js';
+import {
+	resultScreen
+} from './ResultScreen.esm.js';
+import {
+	userData
+} from './UserData.esm.js';
+import {
+	mainMenu
+} from './MainMenu.esm.js';
+import {
+	Sprite
+} from './Sprite.esm.js';
+import {
+	Paddle
+} from './Paddle.esm.js';
+import {
+	keyboardController,
+	KEY_CODE_LEFT,
+	KEY_CODE_PAUSE,
+	KEY_CODE_RIGHT
+} from './KeyboardController.esm.js';
+import {
+	Ball
+} from './Ball.esm.js';
+
+const PLAYER_SPEED = 10;
+
+class Game extends Common {
+	constructor() {
+		super();
+	}
+
+	playLevel(level) {
+		window.removeEventListener(DATALOADED_EVENT_NAME, this.playLevel);
+
+		this.background = new Sprite(0, 33, 800, 450, media.spriteImage, 0, 0);
+		this.paddle = new Paddle();
+		this.ball = new Ball();
+		this.gameState = {
+			isGamePaused: false
+		};
+		// this.gameState = new GameState();
+		this.changeVisibilityScreen(canvas.element, VISIBLE_SCREEN);
+		this.changeVisibilityScreen(mainMenu.miniSettingsLayerElement, VISIBLE_SCREEN);
+		media.isInLevel = true;
+		media.playBackgroundMusic();
+		this.animate();
+	}
+
+	animate() {
+		this.ball.moveAndCheckCollision();
+		this.handleKeyboardClick();
+		this.drawSprites();
+		this.checkEndOfGame();
+	}
+
+	handleKeyboardClick() {
+		const {
+			clickedKey: key
+		} = keyboardController;
+
+		if (!key) {
+			return;
+		}
+
+		if (key === KEY_CODE_PAUSE) {
+			this.gameState.isGamePaused = true;
+			keyboardController.clickedKey = null;
+			return;
+		}
+
+		if (!this.gameState.isGamePaused && key === KEY_CODE_LEFT) {
+			for (let i = PLAYER_SPEED; this.paddle.movePlayerLeft() && i; i--)
+				keyboardController.clickedKey = null;
+			return;
+		}
+
+		if (!this.gameState.isGamePaused && key === KEY_CODE_RIGHT) {
+			for (let i = PLAYER_SPEED; this.paddle.movePlayerRight() && i; i--)
+				keyboardController.clickedKey = null;
+			return;
+		}
+	}
+
+	drawSprites() {
+		this.background.draw(0, 1.25);
+		this.ball.draw();
+		this.paddle.draw();
+	}
+
+	checkEndOfGame() {
+		if (this.ball.hadHitOnBottomEdge()) {
+			media.isInLevel = false;
+			media.stopBackgroundMusic();
+			resultScreen.viewResultScreen(true);
+		} else {
+			this.animationFrame = window.requestAnimationFrame(() => this.animate());
+		}
+	}
+
+	swap(firstDiamond, secondDiamond) {
+		[
+			firstDiamond.kind,
+			firstDiamond.alpha,
+			firstDiamond.match,
+			firstDiamond.x,
+			firstDiamond.y,
+			secondDiamond.kind,
+			secondDiamond.alpha,
+			secondDiamond.match,
+			secondDiamond.x,
+			secondDiamond.y,
+		] = [
+			secondDiamond.kind,
+			secondDiamond.alpha,
+			secondDiamond.match,
+			secondDiamond.x,
+			secondDiamond.y,
+			firstDiamond.kind,
+			firstDiamond.alpha,
+			firstDiamond.match,
+			firstDiamond.x,
+			firstDiamond.y,
+		];
+
+		this.gameState.setIsMoving(true);
+	}
+}
+
+export const game = new Game();
---
+++ Arkanoid/scripts/UserData.esm.js
@@ -0,0 +1,33 @@
+class UserData {
+	constructor() {
+		if (!localStorage.length) {
+			localStorage.setItem('1', JSON.stringify({
+				active: true,
+				bestScore: 0
+			}));
+		}
+	}
+
+	checkAvailabilityLevel(levelNumber) {
+		const item = localStorage.getItem(String(levelNumber));
+
+		if (!item) {
+			return false;
+		}
+
+		const {
+			active
+		} = JSON.parse(item);
+
+		return active;
+	}
+
+	addNewLevel(levelNumber) {
+		localStorage.setItem(String(levelNumber), JSON.stringify({
+			active: true
+		}));
+	}
+
+}
+
+export const userData = new UserData();
